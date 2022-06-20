0fac3004c6bd973f800a52883d21b48b8fe692ff
ABHISHEK <abhishekism0010@gmail.com>
2022-06-21 03:37:38 +0530

    Added particles-bg for moving bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,129 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactGA from 'react-ga';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type ResumeData = {
+  main: {
+    name: string,
+    occupation: string,
+    description: string,
+    image: string,
+    bio: string,
+    contactmessage: string,
+    email: string,
+    address: {
+      street: string,
+      city: string,
+      state: string,
+      zip: string,
+    },
+    website: string,
+    resumedownload: string,
+    social: [
+      {
+        name: string,
+        url: string,
+        className: string,
+      }
+    ]
+  },
+  resume: {
+    skillmessage: string,
+    education: [
+      {
+        school: string,
+        degree: string,
+        graduated: string,
+        description: string
+      }
+    ],
+    work: [
+      {
+        company: string,
+        title: string,
+        years: string,
+        description: string
+      }
+    ],
+    skills: [
+      {
+        name: string,
+        level: string
+      }
+    ]
+  },
+  portfolio: {
+    projects: [
+      {
+        title: string,
+        category: string,
+        image: string,
+        url: string
+      }
+    ]
+  },
+  testimonials: {
+    testimonials: [
+      {
+        text: string,
+        user: string
+      }
+    ]
+  }
 }
+
+interface AppProps {
+};
+
+interface AppState {
+
+  foo: string,
+  resumeData: ResumeData, 
+}
+class App extends React.Component<AppProps, AppState> {
+
+  
+    state: AppState = {
+      foo: 'bar',
+      resumeData: {} as ResumeData
+    };
+
+    constructor(props: AppProps) {
+      super(props);
+      ReactGA.initialize('UA-110570651-1');
+      ReactGA.pageview(window.location.pathname);
+    }
+
+  componentDidMount(){
+    fetch('/resumeData.json')
+    .then( response => response.json())
+    .then( json => {this.setState({resumeData: json});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
+        <Contact data={this.state.resumeData.main}/>
+        <Footer data={this.state.resumeData.main}/>
+      </div>
+    );
+  }
+}
+
+/* Additional Fields can be added to return
+<Portfolio data={this.state.resumeData.portfolio}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        */
 
 export default App;
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+interface FooterProps {
+  data: any
+};
+
+interface FooterState{};
+
+class Footer extends Component<FooterProps, FooterState> {
+  render() {
+
+    if(this.props.data){
+      var networks= this.props.data.social.map(function(network: any){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <footer>
+
+     <div className="row">
+        <div className="twelve columns">
+           <ul className="social-links">
+              {networks}
+           </ul>
+
+           <ul className="copyright">
+              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+           </ul>
+
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+  </footer>
+    );
+  }
+}
+
+export default Footer;

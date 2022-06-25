a0542f89c873ef7fd7e9f8d96740f0433f3c8435
ABHISHEK <abhishekism0010@gmail.com>
2022-06-25 19:45:43 +0530

    Added support to push contact form to email

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -20,7 +20,6 @@ class Contact extends Component<ContactProps, ContactState>{
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
-      var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
@@ -46,27 +45,27 @@ class Contact extends Component<ContactProps, ContactState>{
          <div className="row">
             <div className="eight columns">
 
-               <form action="" method="post" id="contactForm" name="contactForm">
+               <form action="https://formsubmit.co/c8361e6b66f4b043d6a3926811299e6c" method="post" id="contactForm" name="contactForm">
 					<fieldset>
 
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size={35} id="contactName" name="contactName" onChange={this.handleChange}/>
+						   <input required type="text" defaultValue="" size={35} id="contactName" name="contactName" onChange={this.handleChange}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size={35} id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+						   <input required type="text" defaultValue="" size={35} id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size={35} id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
+						   <input required type="text" defaultValue="" size={35} id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols={50} rows={15} id="contactMessage" name="contactMessage"></textarea>
+                     <textarea required cols={50} rows={15} id="contactMessage" name="contactMessage"></textarea>
                   </div>
 
                   <div>
@@ -96,28 +95,6 @@ class Contact extends Component<ContactProps, ContactState>{
 						   <span>{phone}</span>
 					   </p>
 				   </div>
-
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
             </aside>
       </div>
    </section>
